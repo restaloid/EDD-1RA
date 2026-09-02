90cc4246cf3de947c9c3728ec57b9221b8b1c667
restaloid <lucah8023@gmail.com>
2026-09-02 17:53:21 -0600

    Add files via upload

---
+++ EVA1_3_STACKOVERFLOW/src/eva1_3_stackoverflow/EVA1_3_STACKOVERFLOW.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_stackoverflow;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_STACKOVERFLOW { 
+    
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+         B();
+     }
+     public static void B(){
+         A();
+     }
+}
