896aa3f5ac22cdc004b638a81c755592e1072845
restaloid <lucah8023@gmail.com>
2026-09-02 19:47:30 -0600

    Update EVA1_5_REFERENCE.java

--- EVA1_5_REFERENCE/src/eva1_5_reference/EVA1_5_REFERENCE.java
+++ EVA1_5_REFERENCE/src/eva1_5_reference/EVA1_5_REFERENCE.java
@@ -1,33 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package eva1_5_reference;
-
-/**
- *
- * @author Luis Quiñones    
- */
 public class EVA1_5_REFERENCE {
-
-    
     public static void main(String[] args) {
         int x = 5;
-        System.out.println("valor de x=");
+        System.out.println("valor de x = " + x);
         incrementar(x);
         System.out.println("Valor de x modificado = "  + x);  
+        
         //Ahora con objetos
-        Mivalor mv = new MiValor();
+        MiValor mv = new MiValor();
         System.out.println("MiValor.y = " + mv.y);
-        incrementarOjb(mv);
+        incrementarObj(mv);
         System.out.println("MiValor.y =" +mv.y);
     }
     public static void incrementar(int valor){
         valor = valor+1;
         
     }
-    public static void incremetarObj(MiValor valor){
+    public static void incrementarObj(MiValor valor){
         valor.y = valor.y+1;
     }
     
+}
+class MiValor {
+    int y = 5;
 }
